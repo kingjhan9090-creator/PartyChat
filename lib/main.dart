5d978b04d20793e25c52710a0be96a3a3bfdc992
kingjhan9090-creator <kingjhan9090@gmail.com>
2026-09-16 00:49:17 +0400

    Add import for dart:convert in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
