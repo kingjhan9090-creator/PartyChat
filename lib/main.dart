c43de4e02530cea5fea6e333758c6542e7f19c20
kingjhan9090-creator <kingjhan9090@gmail.com>
2026-09-22 04:14:13 +0400

    Alias dart:math as math in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
