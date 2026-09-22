6bdf11ae8e287d3089c09489cf9cfc33658c3990
kingjhan9090-creator <kingjhan9090@gmail.com>
2026-09-22 16:29:12 +0400

    Add firebase_storage import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@
     import 'package:firebase_auth/firebase_auth.dart';
     import 'package:google_sign_in/google_sign_in.dart';
     import 'package:cloud_firestore/cloud_firestore.dart';
+    import 'package:firebase_storage/firebase_storage.dart';
     import 'package:image_picker/image_picker.dart';
     import 'package:flutter/services.dart';
     import 'package:zego_uikit/zego_uikit.dart';
